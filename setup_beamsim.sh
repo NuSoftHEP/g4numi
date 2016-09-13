0ad43de2dafc38e03c576aa4f3589486eea95972
rschroet <rschroet>
2016-09-13 18:35:29 +0000

    Changes to allow building against g4 10_1_p03b; This script is NOvA-specific for now

--- setup_beamsim.sh
+++ setup_beamsim.sh
@@ -1,44 +1,36 @@
 #!/bin/bash
 #-*-Shell-Script-*- #
 
-# $Header: /cvs/projects/numi-beam-sim/numi-beam-sim/g4numi/Attic/setup_beamsim.sh,v 1.1.2.15 2015/11/28 05:08:58 laliaga Exp $
+# $Header: /cvs/projects/numi-beam-sim/numi-beam-sim/g4numi/Attic/setup_beamsim.sh,v 1.1.2.15.2.1 2016/09/13 18:35:29 rschroet Exp $
 
+# This only works on a NOvA virtual machine for now
 
 setup_beamsim(){
-    . "/grid/fermiapp/products/minerva/etc/setups.sh"
+    
+    source /grid/fermiapp/products/nova/externals/setup
+
+    setup geant4 v4_10_1_p03b -q debug:e10
+
+    #dk2nu sets the matching version of ROOT, no need to setup ROOT seperately
+    setup dk2nu v01_04_01 -q debug:e10:r6
+
+    setup cmake v3_0_1
+    
+    WHICH_G4=`ls -d ${GEANT4_FQ_DIR}/lib*/Geant4-*`
+    WHICH_CLHEP=`ls -d ${CLHEP_LIB_DIR}/CLHEP-*`
+
     local TOP=${PWD}
-#   setup -f Linux+2.6-2.5 gcc v3_4_3
 
-# This line sets the modified version of geant4 (that contains a class to store hadronic interaction 
-# kinematics):
-    . /grid/fermiapp/minerva/beamsim/geant4/geant4.9.2.p03_mod/env.sh 
-
-#    setup -q g77-OpenGL -f Linux+2.6 geant4 v4_9_2_p03
-
-    setup -q debug -f Linux+2.6-2.5 root v5_30_00
-
-    if [ -e "/grid/fermiapp/minerva/condor/setup.minerva.condor.sh" ]; then
-	source /grid/fermiapp/minerva/condor/setup.minerva.condor.sh
-    fi
-
-
-    source /grid/fermiapp/products/minerva/etc/setups.sh
-
-    # setup for jobsub client
-    # according to the prescription in Mike Kirby's talk
-    # minerva doc-10551, Dec 2014    
     source /grid/fermiapp/products/common/etc/setups.sh
     setup jobsub_client
     setup ifdhc
 
-    export IFDH_BASE_URI="http://samweb-minerva.fnal.gov:20004/sam/minerva/api"
-
     export BEAMSIM="${TOP}"
     export G4NUMIVER="v6"
-    setup g4photon
-    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:$G4LIB/plists/Linux-g++"
-#    export LD_LIBRARY_PATH="${PWD}/tmpSolution:${LD_LIBRARY_PATH}"
 
+    setup g4photon v3_1
+
+    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:$G4LIB/plists/Linux-g++"
 
     export G4WORKDIR="${TOP}"
     echo "G4WORKDIR is ${G4WORKDIR}"
