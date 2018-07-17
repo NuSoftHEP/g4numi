a5909e282d5ab5e1d4a82688b403c6014b55ed7b
laliaga <laliaga>
2018-07-17 21:45:42 +0000

    script example to send jobs for target optimization

--- run_tgt_opt_fins.sh
+++ run_tgt_opt_fins.sh
@@ -3,23 +3,20 @@
 basedir="/pnfs/nova/scratch/users/${USER}/test_flux"
 
 #####
-njobs="--n_jobs=1"
-pot="--pot=5"
+njobs="--n_jobs=3"
+pot="--pot=100"
 
 outdirs=( "dir1" "dir2" )
 
 #tgt_opt_config=( "--number_of_fins=10 --distance_between_fins=0.25 --target_position_Z=-143.3" "--number_of_fins=20 --distance_between_fins=0.35 --target_position_Z=-163.3" )
-
-tgt_opt_config=( "--number_of_fins=48 --target_position_Z=-143.3" "--number_of_fins=46 --target_position_Z=-138.4" )
+tgt_opt_config=( "--number_of_fins=10 --distance_between_fins=0.25 --target_position_Z=-143.3  --position_budal_monitor=22 --width_fin=14 --winged_fin1=45 --winged_fin2=3 --winged_fin3=67 --winged_fin4=90" )
 
 ####
 exe=ProcessG4NuMI.py
-bspot="--beam_spotsize_X=1.5 --beam_spotsize_Y=1.5"
-bmode="--beamconfig=me000z-200i"
+bspot="--beam_spotsize_X=1.3 --beam_spotsize_Y=1.3"
+bmode="--beamconfig=me000z200i"
 
 len=${#tgt_opt_config[*]} 
-
-echo "len $len"
 
 for (( i=0; i<len; i++ )) 
 do
@@ -27,12 +24,12 @@ do
     mkdir -p ${outdir}
     outdir="--outdir=${outdir}"
     echo "====================================="
-    echo "==>> Sendeiung this commend to the GRID:"
+    echo "==>> Sending this commend to the GRID:"
     echo "python ${exe} ${outdir} ${njobs} ${pot} ${bspot} ${bmode} ${tgt_opt_config[${i}]}"
     echo ""
     echo "====================================="
- python ${exe} ${outdir} ${njobs} ${pot} ${bspot} ${bmode} ${tgt_opt_config[${i}]}
-
+    python ${exe} ${outdir} ${njobs} ${pot} ${bspot} ${bmode} ${tgt_opt_config[${i}]}
+    
 done
  
 
